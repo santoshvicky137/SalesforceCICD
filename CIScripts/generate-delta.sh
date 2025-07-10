bdf7747c97702519a1a23b2404e95509692dd096
santoshvicky137 <114645491+santoshvicky137@users.noreply.github.com>
2025-07-10 18:37:32 +0530

    Update generate-delta.sh

--- CIScripts/generate-delta.sh
+++ CIScripts/generate-delta.sh
@@ -52,13 +52,13 @@ while read -r status file; do
 done < "$INPUT_FILE"
 
 # === STEP 5: Generate package.xml using sf CLI ===
-echo "📦 Generating package.xml using Salesforce CLI..."
+echo "📦 Generating package.xml using Salesforce CLI...and current path is : $(pwd)"
 sf project manifest generate \
   --source-dir "$PACKAGE_DIR" \
   --api-version "$API_VERSION"
   
 echo "📄 Contents of generated package.xml:"
-cat "$PACKAGE_XML"
+cat "$PACKAGE_DIR"
 # === STEP 6: Log included files ===
 echo "📜 Files included in delta package:"
 find "$PACKAGE_DIR" -type f ! -name "package.xml" | sed "s|^$PACKAGE_DIR/|- |"
